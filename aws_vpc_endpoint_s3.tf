b7b8846f7126fc4fe49440e6d83c769bfe70fa17
Andrew McCully <mccullya@gmail.com>
2020-08-20 09:54:59 +0100

    VEN-53: Paramaterising VPC endpoints

--- aws_vpc_endpoint_s3.tf
+++ aws_vpc_endpoint_s3.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_endpoint" "s3" {
+  count             = var.vpc_endpoint_s3_enabled
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
   route_table_ids = [
