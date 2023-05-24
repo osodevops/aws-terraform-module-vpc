3e266df7415904d3b6c7591fe9d821525ade7a06
Andy Singleton <asing@brightprocess.com>
2023-05-24 13:32:57 +0100

    Allow mapping of public IPs to be an option
    Some users require public subnets that do not automatically allocate IPs

--- aws_subnet_public.tf
+++ aws_subnet_public.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = local.public_cidrs[count.index]
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.subnet_map_public_ip_on_launch
 
   tags = merge(
     var.common_tags,
