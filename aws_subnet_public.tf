28373255e7ad7f6a58b6f60ead6dffcf126162c0
MrDevopsman <joshuacallis@icloud.com>
2025-02-24 12:47:54 +0000

    add default false to disable public ip

--- aws_subnet_public.tf
+++ aws_subnet_public.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "public" {
-  count             = 3
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = local.public_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = local.public_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = var.subnet_map_public_ip_on_launch
 
   tags = merge(
     var.common_tags,
--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "common_tags" {
 }
 
 variable "additional_subnet_tags" {
-  type = map(string)
+  type    = map(string)
   default = {}
 }
 
@@ -22,26 +22,31 @@ variable "zone" {
 }
 variable "vpc_endpoint_cloudwatch_enabled" {
   description = "Boolean to turn on/off cloudwatch endpoint"
-  default = 0
+  default     = 0
 }
 variable "vpc_endpoint_cloudwatch_log_enabled" {
   description = "Boolean to turn on/off cloudwatch logs endpoint"
-  default = 0
+  default     = 0
 }
 variable "vpc_endpoint_ec2_enabled" {
   description = "Boolean to turn on/off ec2 endpoint"
-  default = 0
+  default     = 0
 }
 variable "vpc_endpoint_s3_enabled" {
   description = "Boolean to turn on/off s3 endpoint"
-  default = 0
+  default     = 0
 }
 variable "vpc_endpoint_ssm_enabled" {
   description = "Boolean to turn on/off ssm endpoint"
-  default = 0
+  default     = 0
 }
 locals {
   public_cidrs  = [cidrsubnet(var.cidr, 3, 0), cidrsubnet(var.cidr, 3, 1), cidrsubnet(var.cidr, 3, 2)]
   private_cidrs = [cidrsubnet(var.cidr, 3, 3), cidrsubnet(var.cidr, 3, 4), cidrsubnet(var.cidr, 3, 5)]
 }
 
+variable "subnet_map_public_ip_on_launch" {
+  description = "Whether public subnets should allocate a public ip when instances launch"
+  type        = bool
+  default     = false
+}
